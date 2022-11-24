82a215f75f8c7d9264add2a3434f56d713291b20
ShaLi Shaltiel <shalis@google.com>
2022-11-24 11:52:41 +0200

    Initial Test

--- polling-loader.js
+++ polling-loader.js
@@ -1,1 +1,21 @@
+// ==UserScript==
+// @name         Visa Polling
+// @description  Automate polling of earlier appointments
+// @version      0.1
+// @author       You
+// @match        https://ais.usvisa-info.com/en-il/*
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+    var script = document.createElement('script');
+    script.src = 'https://raw.githubusercontent.com/shali3/polling-automation/main/polling.js';
+    
+    script.onload = function () {
+        alert("test1");
+    };
+    script.src = something;
+
+    document.head.appendChild(script); //or something of the likes
+})();
